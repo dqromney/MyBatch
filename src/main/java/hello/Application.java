6a98622d8666f0c91c42407b20259d054433fb57
David Quenten Romney <david.romney@ldschurch.org>
2017-01-30 16:04:58 -0700

    Initial update based upon work in URL: https://spring.io/guides/gs/batch-processing/

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,18 @@
+package hello;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot Application.
+ *
+ * Created by dqromney on 1/30/17.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/hello/Person.java
@@ -0,0 +1,46 @@
+package hello;
+
+/**
+ * Person object.
+ * <p>
+ * Created by dqromney on 1/30/17.
+ */
+public class Person {
+    private String lastName;
+    private String firstName;
+
+    public Person() {
+
+    }
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Person{");
+        sb.append("lastName='").append(lastName).append('\'');
+        sb.append(", firstName='").append(firstName).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+
+}
